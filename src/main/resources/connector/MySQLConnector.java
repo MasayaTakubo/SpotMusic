f700cf5d537834656288546b92d8ab52b6cee23c
Kouta Yokomoto <ky23304020@ga.ttc.ac.jp>
2024-12-03 15:48:18 +0900

    MySQLConnector (getConnとcloseだけ)

---
+++ src/main/resources/connector/MySQLConnector.java
@@ -0,0 +1,43 @@
+package connector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnecter {
+    public static Connection getConn() {
+        Connection conn = null;
+        try{
+            conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/world?characterEncoding=UTF-8&serverTimezone=JST",
+                "info","pro");
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public static void close(ResultSet resultSet, Statement statement, Connection conn) {
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
