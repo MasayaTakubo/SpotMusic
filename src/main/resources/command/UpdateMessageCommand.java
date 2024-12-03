b2717e91db6af0e933647985d42844e30e8f307a
Takuya Satou <ts23304010@ga.ttc.ac.jp>
2024-12-03 15:04:56 +0900

    表示のjspはchatなのでmessageから改名しました。それとBeanのsetterの一部を大文字に変更しました

--- src/main/resources/command/UpdateMessageCommand.java
+++ src/main/resources/command/UpdateMessageCommand.java
@@ -8,12 +8,12 @@ public class UpdateMessageCommand extends AbstractCommand {
         String messageId = messageIds[0];
         String[] sendMessages = reqc.getParameter("sendMessage");//仮。多分jspら辺
         String sendMessage= sendMessages[0];
-        messageBean messageBean = new messageBean();
-        messageBean.setmessageId(messageId);//タンチヤウと相談
-        messageBean.setsendMessage(sendMessage);//タンチヤウと相談
+        messageBean messageBean = new messageBean();//Beanは最初小文字のクラス名なので
+        messageBean.setMessageId(messageId);//タンチヤウと相談
+        messageBean.setSendMessage(sendMessage);//タンチヤウと相談
         MessageDAO messageDAO= new MessageDAO();//チヨウと相談
         messageDAO.UpdateMessage(sendMessages);//チヨウと相談
-        resc.setTarget("message");//多分メッセージ画面に戻すので仮に設定
+        resc.setTarget("chat");//多分メッセージ画面に戻すので仮に設定
         return resc;
     }
 }
